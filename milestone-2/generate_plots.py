6c334eb3e75bba8ed6e49fcbbb5e3182bcdca2cf
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-04-10 13:32:28 -0500

    boundary condition opt

---
+++ milestone-2/generate_plots.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_file(filename):
+    '''
+    Read the file and return the data as a numpy array.
+    '''
+    data = []
+    with open(filename) as f:
+        for row in f:
+            row_data = []
+            for element in row.split(" "):
+                row_data.append(np.float64(element))
+            data.append(row_data)
+    return np.array(data)
+
+def plot_data(filename):
+    '''
+    Plot and save the data.
+    '''
+    data = read_file(filename)
+    plt.imshow(data)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.colorbar(orientation='vertical')
+    plt.title('Advection Simulation Plot')
+    plt.savefig('.' + filename.split('.')[1] + '.png')
+    plt.close()
+
+
+# Visualize and save data for different timestamps in the simulation
+plot_data("./milestone-2/initial_gaussian.txt")
+
+plot_data("./milestone-2/simulation_10000_timesteps.txt")
+
+plot_data("./milestone-2/simulation_15000_timesteps.txt")
+
+plot_data("./milestone-2/simulation_user_specified_timestep.txt")
